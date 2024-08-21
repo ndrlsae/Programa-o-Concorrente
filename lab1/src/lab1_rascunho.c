f2fedc4f3c13dd22936a9cae710fe1d66f41b2c6
a.n.d.r.e.l.u.i.s <89691263+ndrlsae@users.noreply.github.com>
2024-08-20 21:56:35 -0300

    Add files via upload
    os dois códigos são iguais e contêm os mesmos erros, mas o código indicado como rascunho possui prints ao longo do código.

--- lab1/src/lab1_rascunho.c
+++ lab1/src/lab1_rascunho.c
@@ -16,10 +16,11 @@ typedef struct{
 void *soma1(void *arg){
   l_Args args = *(l_Args*) arg;
 
-//  for(int i = 0; i<args.len; i++){ printf("numero %d,   posição %d \n", args.lista[i].Val, args.lista[i].idVector);}
+  printf("lista antes de somar 1:\n");
+  for(int i = 0; i<args.len; i++){ printf("numero %d,   posição %d \n", args.lista[i].Val, args.lista[i].idVector);}
 
   for(int j = args.r; j < args.len; j += args.threads){
-//    printf("Somando 1 ao valor %d na posição %d do vetor \n", args.lista[j].Val, j);
+    printf("Somando 1 ao valor %d na posição %d do vetor \n", args.lista[j].Val, j);
     args.lista[j].Val = args.lista[j].Val + 1;
   }
 
@@ -32,26 +33,29 @@ idNum f_vector(int len){
   for(int i = 0; i < len; i++){
     vector[i].Val = i + 1;
     vector[i].idVector = i;
-//    printf("na posição %d temos %d \n", i, i + 1);
+    printf("na posição %d temos %d \n", i, i + 1);
   }
-
-//  for(int i = 0; i<len; i++){ printf("numero %d,   posição %d \n", vector[i].Val, vector[i].idVector);}
+ printf("lista criada:\n");
+  for(int i = 0; i<len; i++){ printf("numero %d,   posição %d \n", vector[i].Val, vector[i].idVector);}
 
   return *vector;
 }
 
 bool testa(idNum *lista_nova, int len){
   bool resp = true;
+  printf("criando lista para fazer teste com o vetor modificado:\n");
   idNum lista_original = f_vector(len);
   for(int i = 0; i < len; i++){
     if (lista_nova[i].Val - (&lista_original)[i].Val != 1){
       resp = false;
       printf("a lista nova tem %d enquanto a original tinha %d na posição %d \n", lista_nova[i].Val, (&lista_original)[i].Val, i);
+      printf("Fim do teste\n");
       return resp;
     }
 
   }
   printf("Tudo certo\n");
+  printf("Fim do teste\n");
   return resp;
 }
 
@@ -67,12 +71,12 @@ int main(int argc, char* argv[]){
   int M = atoi(argv[1]);
   int N = atoi(argv[2]);
 
-//  printf("M = %d e N = %d \n", M, N);
+  printf("M = %d e N = %d \n", M, N);
 
   idNum Lista = f_vector(N);
 
 
-//  printf("Começar a criação de threads\n");
+  printf("Começar a criação de threads\n");
 
   pthread_t tid_sistema[M];
 
@@ -89,14 +93,14 @@ int main(int argc, char* argv[]){
     args->r = i;
     args->lista = &Lista;
 
-//    printf("Criando a %dª thread\n", i);
+    printf("Criando a thread %d \n", i);
 
     if(pthread_create(&tid_sistema[i], NULL, soma1, (void*) args)){
       printf("Erro na criação da thread %d\n", i);
     }
   }
 
-//  printf("Fim da thread principal \n");
+  printf("Fim da thread principal \n");
 
   testa(&Lista, N);
   pthread_exit(NULL); 
