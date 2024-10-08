88294fb7886041dea2d0a1c794cc38a3ddb51a92
ndrlsae <andreluis@matematica.ufrj.br>
2024-10-08 15:05:08 -0300

    criando versão sem print

--- lab6/wrlock.c
+++ lab6/wrlock.c
@@ -12,37 +12,27 @@ pthread_mutex_t mutex;
 pthread_cond_t cond_read, cond_write;
 
 void init_read(){
-  //printf("leitor: quero ler\n");
   pthread_mutex_lock(&mutex);
   while (write > 0 || q_write > 0){
-    //printf("leitor: %i escritores estão escrevendo/esperando, vou esperar\n", q_write + 1);
     pthread_cond_wait(&cond_read, &mutex);}
   read++;
   pthread_mutex_unlock(&mutex);
-  //printf("leitor: comecei a ler\n");
 }
 
 void finish_read(){
   pthread_mutex_lock(&mutex);
   read--;
-  //printf("leitor: terminei de ler \n");
   if(read == 0){
-    //printf("TODOS os leitores terminaram de ler \n");
     pthread_cond_signal(&cond_write);}
   pthread_mutex_unlock(&mutex);
 }
 
 void init_write(){
-  //printf("escritor: quero escrever\n");
   pthread_mutex_lock(&mutex);
   q_write++;
   while(read > 0 || write > 0){
-//    printf("escritor: %i estão lendo e %i estão escrevendo\n", read, write);
-//    q_write++;
     pthread_cond_wait(&cond_write, &mutex);}
-//    q_write--;}
   q_write--;
-//  printf("escritor: comecei a escrever \n");
   write++;
   pthread_mutex_unlock(&mutex);
 }
@@ -50,10 +40,6 @@ void init_write(){
 void finish_write(){
   pthread_mutex_lock(&mutex);
   write--;
-//  printf("escritor: parei de escrever e deixei %i na fila \n", q_write);
-  
-//  pthread_mutex_unlock(&mutex);
-
   pthread_cond_signal(&cond_write);
   pthread_cond_broadcast(&cond_read);
   pthread_mutex_unlock(&mutex);
